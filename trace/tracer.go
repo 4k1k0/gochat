fca29156e995ec811b77324f909bb37fcfdd7ec3
Wako <dulo.rodrigo@gmail.com>
2019-04-27 22:06:44 -0500

    Agrega trace

---
+++ trace/tracer.go
@@ -0,0 +1,28 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+// Salutation : here you tell us what Salutation is
+// Printer : what is this?
+// Greet : describe what this function does
+// CreateMessage : describe what this function does
+
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
